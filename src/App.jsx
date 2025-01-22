0551a618c2094b8c6f6c213a736781dd1ae288b1
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-01-22 14:07:24 +0100

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalContextProvider } from "./context/globalContext";
-// //LAYOUTS
+
 import DefaultLayout from "./layouts/defaultLayout";
 //PAGES
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
-import People from "./pages/People";
 import NotFound from "./pages/NotFound";
-import VoyageList from "./pages/VoyageList";
-import AllVoyages from "./pages/allVayages";
+
 function App() {
   return (
     <GlobalContextProvider>
       <BrowserRouter>
         <Routes>
-          <Route index Component={HomePage} />
           <Route Component={DefaultLayout}>
+            <Route index Component={HomePage} />
             <Route path="/about" Component={AboutPage} />
-            <Route path="/allVoyages" Component={AllVoyages} />
-            <Route path="/voyage" Component={VoyageList} />
-            <Route path="/:id" Component={People} />
             <Route path="*" Component={NotFound} />
           </Route>
         </Routes>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,5 @@
 import { Link } from "react-router-dom";
 
 export default function Footer() {
-  return (
-    <footer className="footer">
-      <div className="container py-4">
-        <div className="left-footer">
-          <Link to={"/about"}>
-            <span className="fs-5">Viaggi</span>
-          </Link>
-        </div>
-      </div>
-    </footer>
-  );
+  return <footer className="footer"></footer>;
 }
