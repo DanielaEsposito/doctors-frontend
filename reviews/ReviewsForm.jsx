d5c2b27bce29c815134a15a77a00f7b6babb5a79
antonioferrigno28 <ferrigno1@hotmail.it>
2025-01-23 17:40:22 +0100

    working-showdoctor

---
+++ reviews/ReviewsForm.jsx
@@ -0,0 +1,140 @@
+import { useState } from "react";
+
+export default function ReviewsForm({ doctorId, updateReviews }) {
+  const formInitialData = {
+    username: "",
+    email: "",
+    rating: "",
+    text: "",
+  };
+
+  const [reviewForm, setReviewForm] = useState(formInitialData);
+
+  const handleReviewSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm(reviewForm)) {
+      const storeReviewUrl = `http://localhost:3000/${doctorId}/review`;
+
+      const payload = {
+        doctor_id: doctorId,
+        username: reviewForm.username,
+        email: reviewForm.email,
+        rating: parseInt(reviewForm.rating),
+        review_text: reviewForm.text,
+      };
+
+      fetch(storeReviewUrl, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(`Errore API: ${res.status}`);
+          }
+          return res.json();
+        })
+        .then(() => {
+          const getReviewsUrl = `http://localhost:3000/${doctorId}`;
+          fetch(getReviewsUrl)
+            .then((res) => res.json())
+            .then((data) => {
+              updateReviews(data.doctor.reviews);
+            });
+          setReviewForm(formInitialData);
+        })
+        .catch((err) => {
+          console.error("Errore:", err);
+        });
+    } else {
+      alert("Il form non è valido");
+    }
+  };
+
+  const validateForm = ({ username, email, text, rating }) => {
+    if (!username || !email || !text) return false;
+    const voteInt = parseInt(rating);
+    if (isNaN(voteInt) || voteInt < 1 || voteInt > 5) return false;
+    return true;
+  };
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setReviewForm({
+      ...reviewForm,
+      [name]: value,
+    });
+  };
+
+  return (
+    <form
+      className="row g-3 align-items-end mb-3"
+      onSubmit={handleReviewSubmit}
+    >
+      <div className="col-3">
+        <label htmlFor="username" className="form-label">
+          Nome
+        </label>
+        <input
+          type="text"
+          required
+          className="form-control"
+          id="username"
+          name="username"
+          value={reviewForm.username}
+          onChange={handleFormChange}
+        />
+      </div>
+      <div className="col-3">
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          required
+          className="form-control"
+          id="email"
+          name="email"
+          value={reviewForm.email}
+          onChange={handleFormChange}
+        />
+      </div>
+      <div className="col-2">
+        <label htmlFor="rating" className="form-label">
+          Voto (1-5)
+        </label>
+        <input
+          type="number"
+          required
+          className="form-control"
+          id="rating"
+          name="rating"
+          min="1"
+          max="5"
+          value={reviewForm.rating}
+          onChange={handleFormChange}
+        />
+      </div>
+      <div className="col-4">
+        <label htmlFor="text" className="form-label">
+          Testo
+        </label>
+        <textarea
+          className="form-control"
+          id="text"
+          name="text"
+          value={reviewForm.text}
+          required
+          onChange={handleFormChange}
+        ></textarea>
+      </div>
+      <div className="col-2">
+        <button className="btn btn-primary" type="submit">
+          Invia
+        </button>
+      </div>
+    </form>
+  );
+}
---
+++ src/pages/DoctorShowPage.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import ReviewsForm from "../../reviews/ReviewsForm.jsx";
+
+export default function MovieShowPage() {
+  const { id: doctorId } = useParams();
+  const [doctor, setDoctor] = useState({ doctor: {} });
+  const [reviews, setReviews] = useState([]);
+
+  const updateReviews = (newReviews) => {
+    setReviews(newReviews);
+  };
+
+  useEffect(() => {
+    const url = import.meta.env.VITE_BACKEND_URL + "/" + doctorId;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setDoctor(data);
+        setReviews(data.doctor.reviews);
+      });
+  }, []);
+  return (
+    <>
+      <div className="container py-4">
+        {/* stampa nome dottore */}
+        <h1>{doctor.doctor.name}</h1>
+        <h1>{doctor.doctor.surname}</h1>
+        <h1>{doctor.doctor.email}</h1>
+        <h1>{doctor.doctor.city}</h1>
+        <h1>{doctor.doctor.province}</h1>
+        <h1>{doctor.doctor.phone_number}</h1>
+        <h1>{doctor.doctor.address}</h1>
+        <h1>{doctor.doctor.description}</h1>
+
+        <div className="mt-5">
+          <ReviewsForm updateReviews={updateReviews} doctorId={doctorId} />
+          <h2>Recensioni</h2>
+          <div className="row">
+            {reviews.map((review) => (
+              <div className="col-md-4 mb-4" key={review.id}>
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">Voto: {review.rating} ⭐</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                      Recensito da: {review.username}
+                    </h6>
+                    <p className="card-text">"{review.review_text}"</p>
+                  </div>
+                  <div className="card-footer">
+                    <p className="text-muted">
+                      Recensito il:{" "}
+                      {new Date(review.created_at).toLocaleDateString()}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
