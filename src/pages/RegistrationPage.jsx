c62883f73f1bc6575e0c8471f7782d235b48c248
donatimar <donatimar@gmail.com>
2025-01-28 12:24:45 +0100

    modify name

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -11,6 +11,20 @@ const RegistrationForm = () => {
     description: "",
   });
 
+  const specialties = [
+    { id: 1, name: "Anestesiologia" },
+    { id: 2, name: "Cardiologia" },
+    { id: 3, name: "Dermatologia" },
+    { id: 4, name: "Psischiatria" },
+    { id: 5, name: "Endocrinologia" },
+    { id: 6, name: "Medico di Famiglia" },
+    { id: 7, name: "Gastroenterologia" },
+    { id: 8, name: "Geriatria" },
+    { id: 9, name: "Ematologia" },
+    { id: 10, name: "Malattie Infettive" },
+    { id: 11, name: "Polmonologia" },
+  ];
+
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData({
@@ -31,15 +45,15 @@ const RegistrationForm = () => {
         <div className="row mb-3">
           {/* Nome */}
           <div className="col-12 col-md-6 mb-3">
-            <label htmlFor="nome" className="form-label">
+            <label htmlFor="name" className="form-label">
               Nome
             </label>
             <input
               type="text"
               className="form-control"
-              id="nome"
-              name="nome"
-              value={formData.nome}
+              id="name"
+              name="name"
+              value={formData.name}
               onChange={handleChange}
               required
             />
@@ -47,15 +61,15 @@ const RegistrationForm = () => {
 
           {/* Cognome */}
           <div className="col-12 col-md-6 mb-3">
-            <label htmlFor="cognome" className="form-label">
+            <label htmlFor="surname" className="form-label">
               Cognome
             </label>
             <input
               type="text"
               className="form-control"
-              id="cognome"
-              name="cognome"
-              value={formData.cognome}
+              id="surname"
+              name="surname"
+              value={formData.surname}
               onChange={handleChange}
               required
             />
@@ -63,14 +77,14 @@ const RegistrationForm = () => {
 
           {/* Descrizione */}
           <div className="col-12 mb-3">
-            <label htmlFor="descrizione" className="form-label">
+            <label htmlFor="description" className="form-label">
               Descrizione
             </label>
             <textarea
               className="form-control"
-              id="descrizione"
-              name="descrizione"
-              value={formData.descrizione}
+              id="description"
+              name="description"
+              value={formData.description}
               onChange={handleChange}
               rows="3"
               required
@@ -79,31 +93,37 @@ const RegistrationForm = () => {
 
           {/* Specializzazione */}
           <div className="col-12 col-md-6 mb-3">
-            <label htmlFor="specializzazione" className="form-label">
+            <label htmlFor="specialty" className="form-label">
               Specializzazione
             </label>
-            <input
-              type="text"
+            <select
               className="form-control"
-              id="specializzazione"
-              name="specializzazione"
-              value={formData.specializzazione}
+              id="specialty"
+              name="specialty"
+              value={formData.specialty}
               onChange={handleChange}
               required
-            />
+            >
+              <option value="">Seleziona una specializzazione</option>
+              {specialties.map((specialty) => (
+                <option key={specialty.id} value={specialty.id}>
+                  {specialty.name}
+                </option>
+              ))}
+            </select>
           </div>
 
           {/* Indirizzo */}
           <div className="col-12 col-md-6 mb-3">
-            <label htmlFor="indirizzo" className="form-label">
+            <label htmlFor="address" className="form-label">
               Indirizzo
             </label>
             <input
               type="text"
               className="form-control"
-              id="indirizzo"
-              name="indirizzo"
-              value={formData.indirizzo}
+              id="address"
+              name="address"
+              value={formData.address}
               onChange={handleChange}
               required
             />
@@ -127,15 +147,15 @@ const RegistrationForm = () => {
 
           {/* Numero di telefono */}
           <div className="col-12 col-md-6 mb-3">
-            <label htmlFor="telefono" className="form-label">
+            <label htmlFor="phone_number" className="form-label">
               Numero di Telefono
             </label>
             <input
               type="tel"
               className="form-control"
-              id="telefono"
-              name="telefono"
-              value={formData.telefono}
+              id="phone_number"
+              name="phone_number"
+              value={formData.phone_number}
               onChange={handleChange}
               required
             />
@@ -147,7 +167,7 @@ const RegistrationForm = () => {
           type="submit"
           className="btn btn-custom fw-semibold d-inline-block text-start px-3"
         >
-          Registrati
+          Registrazione
         </button>
       </form>
     </div>
