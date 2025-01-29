b693cf43d0c77a951f9fae089157eb2e4984373b
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-01-29 10:15:47 +0100

    add responsive search doctor page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -10,7 +10,7 @@ export default function SearchPage() {
       .then((data) => setSpecialties(data.results));
   }, []);
   console.log(specialties);
-
+  let subtitle = "d-flex";
   const showDoctors = (specialtyId) => {
     fetch(`http://localhost:3000/${specialtyId}/specialties`)
       .then((res) => res.json())
@@ -22,7 +22,7 @@ export default function SearchPage() {
     <div className="wrapper">
       <section id="specialties-tags">
         <div className="container">
-          <h2 className="mb-3">Specialties</h2>
+          <h2 className="mb-3 fw-semibold">Specialties</h2>
           <h4 className="mb-5">
             Choose the specialization you are interested in and browse through
             the various doctors.
@@ -42,21 +42,29 @@ export default function SearchPage() {
       </section>
       <section id="specialties-doctor">
         <div className="container">
-          <div className="row row-cols-3 g-3">
+          {doctor.length === 0 && (
+            <h2 className="text-center my-5 fw-semibold">
+              Learn more about our doctors
+            </h2>
+          )}
+          <div className="row row-cols-lg-4 row-cols-md-2 row-cols-sm-1 g-2  ">
             {doctor.map((doctor) => (
-              <div key={doctor.id} className="col  doctors-row">
-                <div className="card">
+              <div
+                key={doctor.id}
+                className="col d-flex justify-content-center doctors-row mb-3"
+              >
+                <div className="card card-sd">
                   <img
                     src={doctor.image}
                     className="card-img-top"
                     alt={`nome del dottor ${doctor.name}`}
                   />
                   <div className="card-body">
-                    <h5 className="card-text">
+                    <h4 className="card-text">
                       {doctor.name} {doctor.surname}
-                    </h5>
+                    </h4>
                     <h5>{doctor.city}</h5>
-                    <Link to={`/${doctor.id}`} className="tags">
+                    <Link to={`/${doctor.id}`} className="tags ms-0">
                       Show detail
                     </Link>
                   </div>
