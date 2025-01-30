1cd6aaecf9bb4068c1bb476aac9c24a9739cc0f6
donatimar <donatimar@gmail.com>
2025-01-30 17:50:51 +0100

    modified style doctorShowPage

--- src/pages/DoctorShowPage.jsx
+++ src/pages/DoctorShowPage.jsx
@@ -20,10 +20,11 @@ export default function DoctorShowPage() {
         setReviews(data.doctor.reviews);
       });
   }, []);
+
   return (
     <>
       <div className="container py-5">
-        <div className="card doctor-card mb-5 shadow-lg border-0">
+        <div className="card mb-3 shadow-lg border-0">
           <div className="row g-0 align-items-center">
             <div className="col-md-4">
               <img
@@ -65,54 +66,99 @@ export default function DoctorShowPage() {
             </div>
           </div>
         </div>
-        <h3 className="text-custom-dark text-center fw-semibold pb-3">
-          Aggiungi una recensione
-        </h3>
 
-        <div className="mb-3">
-          <ReviewsForm updateReviews={updateReviews} doctorId={doctorId} />
-        </div>
+        <div className="container py-5">
+          <div className="card w-100 shadow-lg border-0">
+            <div className="card-body pb-0">
+              {" "}
+              {/* Rimosso il padding-bottom */}
+              <h3 className="text-custom-dark fw-semibold pb-3 text-center">
+                Aggiungi una recensione
+              </h3>
+              <div className="row">
+                <div className="col-12 col-md-8">
+                  <div className="d-flex justify-content-center"></div>
 
-        <div className="text-center mb-4 pt-2">
-          <h3 className="text-custom-dark fw-semibold">Media Recensioni</h3>
-          <h1 className="text-warning pt-2">
-            {reviews.length > 0
-              ? (
-                  reviews.reduce((acc, review) => acc + review.rating, 0) /
-                  reviews.length
-                ).toFixed(1)
-              : "Nessuna recensione"}
-            <i className="fa-solid fa-star"></i>
-          </h1>
-        </div>
-
-        <h2 className="text-custom-dark text-center mb-4 fw-semibold">
-          Recensioni
-        </h2>
-        <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3">
-          {reviews.map((review) => (
-            <div className="mb-4" key={review.id}>
-              <div className="card border-0 shadow h-100">
-                <div className="card-body">
-                  <h5 className="card-title">
-                    <span className="text-custom-dark">Voto:</span>{" "}
-                    {review.rating}{" "}
-                    <i className="fa-solid fa-star text-warning"></i>
-                  </h5>
-                  <h6 className="card-subtitle mb-2 text-custom-muted">
-                    Recensito da: {review.username}
-                  </h6>
-                  <p className="card-text">"{review.review_text}"</p>
+                  <div className="mb-3">
+                    <ReviewsForm
+                      updateReviews={updateReviews}
+                      doctorId={doctorId}
+                    />
+                  </div>
                 </div>
-                <div className="card-footer bg-light">
-                  <small className="text-custom-muted">
-                    Recensito il:{" "}
-                    {new Date(review.created_at).toLocaleDateString()}
-                  </small>
+
+                <div className="col-12 col-md-4 d-flex align-items-start">
+                  <img
+                    src="/dottore.png"
+                    alt="Dottore"
+                    className="img-fluid review-img d-none d-md-block"
+                  />
                 </div>
               </div>
             </div>
-          ))}
+          </div>
+        </div>
+
+        <div className="container py-3">
+          <div className="card w-100 shadow-lg border-0">
+            <div className="card-body">
+              {/* Media Recensioni */}
+              <div className="text-center mb-4 pt-2">
+                <span className="badge border border-warning shadow-sm bg-white text-warning fs-3 px-3 py-2 d-inline-flex align-items-center">
+                  <h3 className="mb-0 me-2 text-custom-dark fw-semibold">
+                    Media Recensioni:
+                  </h3>
+                  <h1 className="mb-0">
+                    {reviews.length > 0
+                      ? (
+                          reviews.reduce(
+                            (acc, review) => acc + review.rating,
+                            0
+                          ) / reviews.length
+                        ).toFixed(1)
+                      : "Nessuna recensione"}
+                  </h1>
+                  <i className="fa-solid fa-star ms-2"></i>
+                </span>
+              </div>
+
+              {/* Titolo Recensioni */}
+              <h2 className="text-custom-dark text-center mb-4 fw-semibold">
+                Recensioni
+              </h2>
+
+              {/* Recensioni */}
+              <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3">
+                {reviews.map((review) => (
+                  <div className="mb-4" key={review.id}>
+                    <div className="card border-0 shadow-lg h-100">
+                      <div className="card-body">
+                        {/* Voto */}
+                        <h5 className="card-title">
+                          <span className="text-custom-dark">Voto:</span>{" "}
+                          {review.rating}{" "}
+                          <i className="fa-solid fa-star text-warning"></i>
+                        </h5>
+                        {/* Recensito da */}
+                        <h6 className="card-subtitle mb-2 text-custom-muted">
+                          Recensito da: {review.username}
+                        </h6>
+                        {/* Testo recensione */}
+                        <p className="card-text">"{review.review_text}"</p>
+                      </div>
+                      {/* Data di recensione */}
+                      <div className="card-footer bg-light">
+                        <small className="text-custom-muted">
+                          Recensito il:{" "}
+                          {new Date(review.created_at).toLocaleDateString()}
+                        </small>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
